5cdfa57556dec65203411288cd9e6d9c2bd3569e
Yuliia Zubenko <95251336+yuliiazubenko@users.noreply.github.com>
2024-11-06 19:14:45 +0100

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesObject = {};
+
+  sourceString
+    .split('\n')
+    .map((line) => line.trim())
+    .filter((line) => line && line.includes(':'))
+    .forEach((line) => {
+      const [key, value] = line.split(':').map((part) => part.trim());
+
+      if (key && value) {
+        if (value.endsWith(';')) {
+          // eslint-disable-next-line no-const-assign
+          value = value.slice(0, -1);
+        }
+        stylesObject[key] = value;
+      }
+    });
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;
